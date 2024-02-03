74b21d349831f26068eb6a7d2304812e42e56858
RoboJunior <119618019+RoboJunior@users.noreply.github.com>
2024-02-03 19:38:19 +0000

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "Bengaluru, India",
+        "salary": "Rs. 10,00,000"
+    },
+    {
+        "id": 2,
+        "title": "Data Scientist",
+        "location": "Delhi, India",
+        "salary": "Rs. 15,00,000"
+    },
+    {
+        "id": 3,
+        "title": "Ai Developer",
+        "location": "Bengaluru, India",
+        "salary": "Rs. 55,00,000"
+    },
+    {
+        "id": 4,
+        "title": "Backend Developer",
+        "location": "Chennai, India",
+        "salary": "Rs. 5,00,000"
+    },
+]
 
 
 @app.route("/")
 def hello():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS, company_name="JrTech")
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
